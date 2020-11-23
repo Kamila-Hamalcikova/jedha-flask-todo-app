d515822c9c7b52200983006e2a90d49176a8fddb
Kamila Hamalcikova <Kamila.Hamalcikova@gmail.com>
2020-11-23 15:31:46 +0100

    adding features of todo list

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 DATABASE = "./todo.db"
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////" + DATABASE
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + DATABASE
 db = SQLAlchemy(app)
 
 
@@ -19,17 +19,27 @@ def index():
     todo_list = Todo.query.all()
     return render_template("index.html", todo_list=todo_list)
 
-
+@app.route("/add", methods=["POST"])
 def add():
-    pass
+    title = request.form.get("title")
+    new_todo = Todo(title=title, complete=False)
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for("index"))
 
-
+@app.route("/complete/<string:todo_id>")
 def complete(todo_id):
-    pass
+    todo = Todo.query.filter_by(id=todo_id).first()
+    todo.complete = not todo.complete
+    db.session.commit()
+    return redirect(url_for("index"))
 
-
+@app.route("/delete/<string:todo_id>")
 def delete(todo_id):
-    pass
+    todo = Todo.query.filter_by(id=todo_id).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for("index"))
 
 
 if __name__ == "__main__":
